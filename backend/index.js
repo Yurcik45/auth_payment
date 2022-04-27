8a701d0b4c8d12f89b3fdc7e27efcf8455a57db8
yurcik45 <yurcik45mail@gmail.com>
2022-04-28 01:50:26 +0300

    add initial backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const PORT = process.env.PORT || 4000;
+const express = require("express");
+const app = express();
+
+app.use(express.json({ extended: true }));
+app.use(cors());
+//app.use('/api', require('./routes/index'));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("it is auth_payment backend"));
+
+app.listen(PORT, () => console.log(`-- server started on port ${PORT} --`));
